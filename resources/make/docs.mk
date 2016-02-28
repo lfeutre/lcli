dad2edbf60d44195122cae6894e5e450c5bca429
Duncan McGreggor <oubiwann@gmail.com>
2016-02-28 16:03:50 -0600

    Cleaned up docs make include.

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -1,29 +1,11 @@
 .PHONY: docs api-docs user-guide publish-docs clean-docs
 
 DOCS_DIR = $(ROOT_DIR)/docs
-USER_GUIDE_DIR = $(DOCS_DIR)/user-guide
-
-# The following dir is for build only and can be destroyed
-DOCS_BUILD_DIR = $(DOCS_DIR)/build
-
-# The following dir is tracked by the project git (not the ephemeral git repo
-# created for pushing to gh-pages) and CANNOT BE DESTROYED -- this is where the
-# versioned docs live (i.e., all past releases of documentation, both API and
-# user-guide).
-#
-# The layout of the archived docs is as follows (relative to DOCS_DIR/master):
-#   index.html -> meta-refresh to current
-#   current/index.html -> links to current/api/ and current/user-guide/
-#   x.y.z/index.html -> links to x.y.z/api/ and x.y.z/user-guide/
-#
-# Note that /current is not tracked in the repo (there is a .gitignore for
-# docs/master/current. Everything else in the layout above, however, is tracked
-# in git.
 CURRENT_VERSION = current
-DOCS_PROD_DIR = $(DOCS_DIR)/master
+DOCS_PROD_DIR = $(DOCS_DIR)
 CURRENT_PROD_DIR = $(DOCS_PROD_DIR)/$(CURRENT_VERSION)
 
-docs: clean-docs $(DOCS_BUILD_DIR) proj-docs
+docs: clean-docs proj-docs
 
 publish-docs: $(DOCS_PROD_DIR) docs setup-temp-repo
 	@echo "\nPublishing docs ...\n"
@@ -35,56 +17,26 @@ publish-docs: $(DOCS_PROD_DIR) docs setup-temp-repo
 proj-docs:
 	@echo "\nBuilding project docs ...\n"
 	@rm -rf $(CURRENT_PROD_DIR)
+	@mkdir -p $(DOCS_PROD_DIR)
 	@rebar3 lfe lodox
-	@mv doc $(CURRENT_PROD_DIR)
 
-SLATE_GIT_HACK = $(DOCS_DIR)/.git
-
-$(DOCS_BUILD_DIR):
-	@mkdir -p $(DOCS_BUILD_DIR)
-
-$(GUIDE_BUILD_DIR):
-	@mkdir -p $(GUIDE_BUILD_DIR)
-	@make $(USER_GUIDE_DIR)/build
-
-$(USER_GUIDE_DIR)/build:
-	@ln -s $(GUIDE_BUILD_DIR) $(USER_GUIDE_DIR)/build
+DOCS_GIT_HACK = $(DOCS_DIR)/.git
 
 $(DOCS_PROD_DIR):
 	@mkdir -p $(DOCS_PROD_DIR)
 
-$(SLATE_GIT_HACK):
-	@make $(USER_GUIDE_DIR)/.git
+$(DOCS_GIT_HACK):
+	@ln -s $(ROOT_DIR)/.git $(DOCS_GIT_HACK)
 
-$(USER_GUIDE_DIR)/.git:
-	@ln -s $(ROOT_DIR)/.git $(USER_GUIDE_DIR)
+# $(USER_GUIDE_DIR)/.git:
+# 	@ln -s $(ROOT_DIR)/.git $(USER_GUIDE_DIR)
 
-user-guide-setup:
-	@echo "\nInstalling and setting up dependencies ..."
-	@cd $(USER_GUIDE_DIR) && bundle install
-
-user-guide-dev:
-	@echo "\nRunning development server ..."
-	@cd $(USER_GUIDE_DIR) && bundle exec middleman server
-
-clean-docs:
-	@echo "\nCleaning build directory ..."
-	@rm -rf $(DOCS_BUILD_DIR)
-
-user-guide: $(SLATE_GIT_HACK)
-	@echo "\nBuilding user guide ...\n"
-	@cd $(USER_GUIDE_DIR) && bundle exec middleman build --clean
-
-setup-temp-repo: $(SLATE_GIT_HACK)
+setup-temp-repo:
 	@echo "\nSetting up temporary git repos for gh-pages ...\n"
-	@rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git $(DOCS_PROD_DIR)/*/*/.git
-	@mkdir -p $(CURRENT_PROD_DIR)
-	@cp -r $(DOCS_BUILD_DIR)/* $(CURRENT_PROD_DIR)
 	@cd $(DOCS_PROD_DIR) && git init
 	@cd $(DOCS_PROD_DIR) && git add * > /dev/null
 	@cd $(DOCS_PROD_DIR) && git commit -a -m "Generated content." > /dev/null
 
 teardown-temp-repo:
 	@echo "\nTearing down temporary gh-pages repos ..."
-	@rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/*/.git $(DOCS_PROD_DIR)/*/*/.git
-
+	@rm $(DOCS_PROD_DIR)/.git
